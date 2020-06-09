7a47532a1b7dada246dc8decf36c6172eb6ce154
Adela <adela.matouskova1@gmail.com>
2020-06-09 21:46:07 +0200

    Middleware rename to controllers

--- routes/decks.js
+++ routes/decks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllDecks, getDeck, createDeck, updateDeck, deleteDeck } = require('../middleware/decks');
+const { getAllDecks, getDeck, createDeck, updateDeck, deleteDeck } = require('../controllers/decks');
 
 router.route('/').get(getAllDecks);
 router.route('/:id').get(getDeck).post(createDeck).put(updateDeck).delete(deleteDeck);
