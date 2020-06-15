44c8c843546d72aa3c1f99237cb20f834687ab04
Adela <adela.matouskova1@gmail.com>
2020-06-15 14:25:47 +0200

    Seeder

---
+++ seeder.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const Deck = require('./models/Deck');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const decks = JSON.parse(fs.readFileSync(`${__dirname}/_data/decks.json`, 'utf-8'));
+
+const importData = async () => {
+  try {
+    await Deck.create(decks);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Deck.deleteMany();
+
+    console.log('Data Deleted...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
