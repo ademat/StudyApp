91d27d8c1b160f319f758c5b390a0224f32c3e3e
Adela <adela.matouskova1@gmail.com>
2020-06-15 16:30:55 +0200

    Filter, select, sort Deck

--- controllers/decks.js
+++ controllers/decks.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const ErrorResponse = require('../utils/errorResponse');
 const Deck = require('../models/Deck');
 
@@ -6,7 +7,41 @@ const Deck = require('../models/Deck');
 // @access private
 exports.getAllDecks = async (req, res, next) => {
   try {
-    const decks = await Deck.find();
+    let query;
+
+    // copy req.query
+    const reqQuery = { ...req.query };
+
+    // Fields to exclude from filtering
+    const removeFields = ['select', 'sort'];
+    removeFields.forEach((param) => delete reqQuery[param]);
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    // create operators ($gt, $gte...)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
+
+    // finding documents
+    query = Deck.find(JSON.parse(queryStr));
+
+    // Select fields
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' ');
+      query = query.select(fields);
+    }
+
+    // Sort by parameters, default by Deck name
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('name');
+    }
+
+    // Executing query
+    const decks = await query;
+
     res.status(200).json({ succes: true, data: decks });
   } catch (err) {
     next(err);
