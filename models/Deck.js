6385d2cdb36144621478c2bdc631c5685d802b61
Adela <adela.matouskova1@gmail.com>
2020-06-15 12:28:49 +0200

    Create slug from Deck name

--- models/Deck.js
+++ models/Deck.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const DeckSchema = new mongoose.Schema({
   name: {
@@ -7,9 +8,16 @@ const DeckSchema = new mongoose.Schema({
     maxlength: 20,
     required: [true, 'Please add Deck name'],
   },
+  slug: String,
   countAll: Number,
   countNew: Number,
   countReview: Number,
+});
+
+// Create deck slug from the name
+DeckSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Deck', DeckSchema);
